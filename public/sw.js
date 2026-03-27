f73a0dce81d1d600bffa2b86e6573ff62bf5518b
‘Lokesh <lokesh.cdewanand@gmail.com>
2026-03-27 15:27:47 +0530

    Notification : service worker

---
+++ public/sw.js
@@ -0,0 +1,8 @@
+self.addEventListener("push", (event) => {
+	const data = event.data?.json() || {};
+	event.waitUntil(
+		self.registration.showNotification(data.title || "Notification", {
+			body: data.body || "Test",
+		}),
+	);
+});
